c930594e493023a03876a64791090f96933a1b75
Mark Tomlinson <mark.tomlinson3@gmail.com>
2020-11-10 19:19:50 +1100

    end of lesson 8

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 // Eleventy config file
 // markdown files, data files and HTML files should be processed by Nunjucks
 // That means that we can now use .html files instead of having to use .njk files.
+// .addPassthroughCopy that's how we get the images to the dist folder
 
 
 module.exports = config => {
+  config.addPassthroughCopy('./src/images/');
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
